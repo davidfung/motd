2a669d163d7838e1d8ec2c33b97a8d72ac1b8060
davidfung <davidfung@amgcomputing.com>
2026-03-08 00:59:33 -0800

    1st runnable version

--- motd.go
+++ motd.go
@@ -1,5 +1,21 @@
 package motd
 
+import (
+	_ "embed"
+	"math/rand"
+	"strings"
+)
+
+//go:embed motd.txt
+var s string
+
 func Next() string {
-    return "hooray!"
+	ss := strings.Split(s, "\n")
+	for {
+		r := rand.Intn(len(ss))
+		t := ss[r]
+		if strings.TrimSpace(t) != "" {
+			return t
+		}
+	}
 }
