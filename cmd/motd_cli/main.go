ba8b364f08b6dda2860e846df02ed3726244cf1b
davidfung <davidfung@amgcomputing.com>
2026-06-04 13:59:27 -0700

    Fix the version tag to conform with go standard

--- cmd/motd_cli/main.go
+++ cmd/motd_cli/main.go
@@ -1,6 +1,10 @@
-package main
-
+// To build the command line tool manually:
 // $ go build -o motd
+//
+// To install it as a Go tool:
+// $ go install github.com/davidfung/motd/cmd/motd_cli@latest
+
+package main
 
 import (
 	"fmt"
